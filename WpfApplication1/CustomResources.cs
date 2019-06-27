74a71f5ae4747438c2a97bfb61848ea4a348b3bc
Simon Stanford <simonjstanford@gmail.com>
2019-06-27 07:02:22 +0100

    initial commit

---
+++ WpfApplication1/CustomResources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Library
+{
+    public class CustomResources
+    {
+        public static ComponentResourceKey CustomColour
+        {
+            get
+            {
+                return new ComponentResourceKey(typeof(CustomResources), "CustomColour");
+            }
+        }
+    }
+}
